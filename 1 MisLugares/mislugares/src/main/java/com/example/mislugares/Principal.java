beb8d5f895b5020d7fd72f449efec808401e188a
german <germanjongewaard@gmail.com>
2016-08-15 17:01:34 +0200

    El gran libro de Android
    Prácticas de: "El gran libro de Android" 5ª Edición - Autor: Jesús Tomás
    
    5ª edición - versión 6.0 Marshmallow - Android Studio.

---
+++ 1 MisLugares/mislugares/src/main/java/com/example/mislugares/Principal.java
@@ -0,0 +1,16 @@
+package com.example.mislugares;
+
+/**
+ * Created by german on 8-8-16.
+ */
+public class Principal {
+    public static void main(String[] main){
+
+
+        for (Lugar l : Lugares.vectorLugares){
+            System.out.println(l);
+        }
+
+    }
+}
+
